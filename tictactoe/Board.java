d988904313c934e5123fc6ee00a4f95e1823b110
DanielBoerlage <daniel.brlg@gmail.com>
2014-10-17 15:01:31 -0700

    layed out tictactoe beginnings

---
+++ tictactoe/Board.java
@@ -0,0 +1,16 @@
+package tictactoe;
+
+public class Board {
+
+	public static final int EMPTY = 0, X = 1, O = 2;
+
+	int[][] grid;
+
+	public Board() {
+		grid = new int[3][3];
+	}
+
+	public static void draw() {
+		System.out.print("hi");
+	}
+}
