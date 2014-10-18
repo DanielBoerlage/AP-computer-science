07d8139d1124d2dfb84894d90fa40bdf4812d037
DanielBoerlage <daniel.brlg@gmail.com>
2014-10-18 15:40:22 -0700

    started working with lanterna and got swing terminal emulator to work

--- tictactoe/Board.java
+++ tictactoe/Board.java
@@ -1,16 +1,48 @@
 package tictactoe;
 
+import com.googlecode.lanterna.terminal.swing.SwingTerminal;
+
 public class Board {
 
-	public static final int EMPTY = 0, X = 1, O = 2;
-
-	int[][] grid;
+	char[][] grid;  // [row] [column]
 
 	public Board() {
-		grid = new int[3][3];
+		grid = new char[3][3];
+		for(int i = 0; i < 9; i++)
+			grid[i/3][i%3] = ' ';
+		grid[1][1] = 'X';
 	}
 
-	public static void draw() {
-		System.out.print("hi");
+	/*
+
+	 | |
+	-+-+-
+	 | |
+    -+-+-
+     | |
+
+	*/
+
+	public void draw(SwingTerminal terminal) {
+		String horizBar = "-+-+-";
+		String print = row(0) + horizBar + row(1) + horizBar + row(2);
+		putStr(terminal, row(0));
+		terminal.moveCursor(0, 1);
+		putStr(terminal, horizBar);
+		terminal.moveCursor(0, 2);
+		putStr(terminal, row(1));
+		terminal.moveCursor(0, 3);
+		putStr(terminal, horizBar);
+		terminal.moveCursor(0, 4);
+		putStr(terminal, row(2));
+	}
+
+	private String row(int c) {
+		return grid[0][c] + "|" + grid[1][c] + "|" + grid[2][c];
+	}
+
+	private void putStr(SwingTerminal terminal, String str) {
+		for(int i = 0; i < str.length(); i++)
+			terminal.putCharacter(str.charAt(i));
 	}
 }
--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,21 +1,21 @@
 package tictactoe;
 
-import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.swing.SwingTerminal;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Terminal terminal = TerminalFacade.createTerminal();
-		terminal.enterPrivateMode();
-		terminal.moveCursor(10, 5);
-    	terminal.putCharacter('H');
-    	terminal.putCharacter('e');
-    	terminal.putCharacter('l');
-    	terminal.putCharacter('l');
-    	terminal.putCharacter('o');
-    	terminal.putCharacter('!');
-    	terminal.moveCursor(0, 0);
-    	terminal.exitPrivateMode();
+		//Terminal terminal = TerminalFacade.createTerminal();
+        SwingTerminal terminal = TerminalFacade.createSwingTerminal();
+        terminal.enterPrivateMode();
+        JFrame frame = terminal.getJFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Tic tac toe");
+        terminal.setCursorVisible(false);
+        Board board = new Board();
+        board.draw(terminal);
 	}
 }
