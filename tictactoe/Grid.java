4ab2118ee0dd4fa047585cf567ca71e7400cb336
DanielBoerlage <daniel.brlg@gmail.com>
2014-10-22 00:17:45 -0700

    changed around the location of board and introduced grid; started computer Ai algorithm

---
+++ tictactoe/Grid.java
@@ -0,0 +1,58 @@
+package tictactoe;
+
+class Grid {
+
+	private char[][] grid;
+
+	public Grid() {
+		grid = new char[3][3];
+	}
+
+	public Grid(Grid copyFrom) {
+		grid = new char[3][3];
+		for(int i = 0; i < 9; i++)
+			put(copyFrom.get(i), i);
+	}
+
+	public void reset() {
+		for(int i = 0; i < 9; i++) 
+			put(' ', i);
+	}
+
+	public char get(int position) {
+		return grid[position % 3][position / 3];
+	}
+
+	public void put(char c, int position) {
+		grid[position % 3][position / 3] = c;
+	}
+
+	public Grid putAndPop(char c, int position) {
+		Grid out = new Grid(this);
+		out.put(c, position);
+		return out;
+	}
+
+	public char[] flatten() {
+		char[] out = new char[9];
+		for(int i = 0; i < 9; i++)
+			out[i] = get(i);
+		return out;
+	}
+
+	public boolean isFull() {
+		for(int i = 0; i < 9; i++) {
+			if(get(i) == ' ')
+				return false;
+		}
+		return true;
+	}	
+
+	public boolean win() {
+		return false;
+	}
+
+	public boolean lose() {
+		return false;
+	}
+}
