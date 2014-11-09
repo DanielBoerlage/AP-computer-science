3b2c50e2b89a04bfd7e3a6c19e5946ab4f679026
DanielBoerlage <daniel.brlg@gmail.com>
2014-11-09 14:39:58 -0800

    pretty much done and can scan dictionary

--- chemical_words/StringProcessor.java
+++ chemical_words/StringProcessor.java
@@ -10,15 +10,29 @@ public class StringProcessor {
 			if(dict.hasDouble(str.charAt(i), str.charAt(i+1)))
 				doubles.add(i);
 		}
+//		System.out.println(doubles);
 		for(Combinatorics.Combination combination : Combinatorics.getCombinations(doubles.size())) {
 			Element[] elements = new Element[str.length() - combination.getSum()];
-			for(int strI = 0, elementI = 0; strI < str.length();) {
-				if(doubles.contains(strI) && combination.getStateAt(doubles.indexOf(strI)) {
+			for(int strI = 0, elementI = 0; strI <= str.length(); elementI++) {
+//				System.out.println(strI);
+				if(strI >= str.length() || elementI >= elements.length) {
+//					System.out.println("found word!");
+					return new ElementWord(elements);
+
+				}
+				if(doubles.contains(strI) && combination.getStateAt(doubles.indexOf(strI))) {
 					elements[elementI] = dict.getElement(str.substring(strI, strI + 2));
-					elementI++;
+//					System.out.println(str.substring(strI, strI + 2) + "  " + dict.getElement(str.substring(strI, strI + 2)));
 					strI+= 2;
-				} else 
+//					System.out.println("double");
+				} else if(dict.hasSingle(str.charAt(strI))) {
+					elements[elementI] = dict.getElement(str.substring(strI, strI + 1));
+					strI+= 1;
+//					System.out.println("single");
+				} else
+					break;
 			}
 		}
+		return new ElementWord();
 	}
 }
